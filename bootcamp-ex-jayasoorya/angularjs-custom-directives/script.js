3f9cc43c6867d1eebfe409ce05eff1225900f0fa
nishnthini.t <nishanthini.t@habile.in>
2022-03-25 08:21:02 +0530

    Custom directives changed

--- bootcamp-ex-jayasoorya/angularjs-custom-directives/script.js
+++ bootcamp-ex-jayasoorya/angularjs-custom-directives/script.js
@@ -1,7 +1,17 @@
-var app = angular.module('mainApp', []);
+var app = angular.module("PopupDemo", ['ui.bootstrap']);
 
-app.directive("firstDirective", function(){
-    return{
-        template: '<span>Click Here</span>'
-    };
+app.directive("firstDirective", function () {
+  return {
+    template: "<span>Click Here For Popup</span>",
+  };
 });
+
+angular.module("PopupDemo").controller("PopupDemoCont", ["$scope","$modal",function ($scope, $modal) {
+    $scope.open = function () {
+      console.log("opening pop up");
+      var modalInstance = $modal.open({
+        templateUrl: "popup.html",
+      });
+    };
+  },
+]);
