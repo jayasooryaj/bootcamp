807268ad831ed20582981efeceade56a8546573e
jayasooryaj <jayasoorya.j@habile.in>
2022-03-28 18:19:17 +0530

    Decorator Pattern

---
+++ bootcamp-ex-jayasoorya/src/designpatterns/DecoratorDemo.java
@@ -0,0 +1,47 @@
+package designpatterns;
+
+public class DecoratorDemo {
+    public static void main(String[] args) {
+        Shape circle = new Circle();
+        Shape redCircle = new RedShapeDecorator(new Circle());
+        Shape redRectangle = new RedShapeDecorator(new Rectangle());
+
+        System.out.println("Circle with normal border");
+        circle.draw();
+
+        System.out.println("\nCircle of red border");
+        redCircle.draw();
+
+        System.out.println("\nRectangle of red border");
+        redRectangle.draw();
+    }
+}
+
+abstract class ShapeDecorator implements Shape{
+    protected Shape decoratedShape;
+
+    public ShapeDecorator(Shape decoratedShape){
+        this.decoratedShape = decoratedShape;
+    }
+
+    public void draw(){
+        decoratedShape.draw();
+    }
+}
+
+class RedShapeDecorator extends ShapeDecorator{
+
+    public RedShapeDecorator(Shape decoratedShape) {
+        super(decoratedShape);
+    }
+
+    @Override
+    public void draw() {
+        decoratedShape.draw();
+        setRedBorder(decoratedShape);
+    }
+    private void setRedBorder(Shape decoratedShape){
+        System.out.println("Border color is red");
+    }
+}
+
